67b1b39d2ec86aecbf3621fdb52d352901028118
hellomac87 <hellomac87@gmail.com>
2018-09-11 18:59:44 +0900

    프로젝트 구조 잡기

--- README.md
+++ README.md
@@ -34,3 +34,19 @@ $ yarn add node-sass sass-loader classnames
 $ yarn add react-router-dom redux redux-actions react-redux redux-pender immutable
 ```
 ##### 루트 컴포넌트 설정
+
+##### 리덕스 설정
+리덕스를 설정하려면 프로젝트에서 필요한 모듈들을 먼저 만들어야 한다. 프로젝트에 필요한 모듈은 네 종류이다.
+- base : 로그인 상태, 삭제 및 로그인 할 때 보이는 모달 생타를 다룬다.
+- editor : 마크다운 에디터 상태를 다룬다.
+- list : 포스트 목록 상태를 다룬다.
+- post : 단일 포스트 상태를 다룬다.
+
+##### 라우트 지정
+프로젝트에 필요한 라우트에서 사용할 페이지 컴포넌트. 총 여섯 종류
+1. 홈
+2. 포스트 목록
+3. 포스트 목록(태그 설정)
+4. 포스트 읽기
+5. 에디터
+6. 404 페이지
---
+++ src/store/configure.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import penderMiddleware from 'redux-pender';
+import * as modules from './modules';
+
+const reducers = combineReducers(modules);
+const middlewares = [penderMiddleware()];
+
+// 개발 모드일 때만 Redux Devtools를 적용합니다.
+const isDev = process.env.NODE_ENV === 'development';
+const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devtools || compose;
+
+// preloadState는 추후 서버사이드 렌더링 했을 때 전달받는 오기 상태입니다.
+const configure = (preloadState) => createStore(reducers, preloadState, composeEnhancers(applyMiddleware(...middlewares)));
+
+export default configure;
