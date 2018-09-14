972b3f24eac2b539ee2a02f00a156c51599c7659
hellomac87 <hellomac87@gmail.com>
2018-09-14 19:34:54 +0900

    PreviewPane 컴포넌트

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import EditorTemplate from "components/editor/EditorTemplate/EditorTemplate";
 import EditorHeader from "components/editor/EditorHeader/EditorHeader";
 import EditorPane from "components/editor/EditorPane/EditorPane";
+import PreviewPane from "components/editor/PreviewPane/PreviewPane";
 
 const EditorPage = () => {
     return (
         <EditorTemplate
             header={<EditorHeader/>}
             editor={<EditorPane/>}
-            preview="프리뷰"
+            preview={<PreviewPane/>}
         />
     );
 };
