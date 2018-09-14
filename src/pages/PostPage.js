1be5989a54e9e439ce761c6e39aa76f500f03bf5
hellomac87 <hellomac87@gmail.com>
2018-09-14 18:34:30 +0900

    PostBody 컴포넌트

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import PostInfo from "components/post/PostInfo/PostInfo";
+import PostBody from "components/post/PostBody/PostBody";
 
 const PostPage = () => {
     return (
         <PageTemplate>
             <PostInfo/>
+            <PostBody/>
         </PageTemplate>
     );
 };
