9116c8ff98b11e6b7cdc3b399e6a454ca4d52571
hellomac87 <hellomac87@gmail.com>
2018-09-16 10:58:16 +0900

    EditorPaneContainer 작성

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import EditorTemplate from "components/editor/EditorTemplate/EditorTemplate";
 import EditorHeader from "components/editor/EditorHeader/EditorHeader";
-import EditorPane from "components/editor/EditorPane/EditorPane";
+import EditorContainer from "containers/editor/EditorPaneContainer";
 import PreviewPane from "components/editor/PreviewPane/PreviewPane";
 
 const EditorPage = () => {
     return (
         <EditorTemplate
             header={<EditorHeader/>}
-            editor={<EditorPane/>}
+            editor={<EditorContainer/>}
             preview={<PreviewPane/>}
         />
     );
