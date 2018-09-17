7ab92429e0ad8993e62ae9d4ba91464f65ed1031
hellomac87 <hellomac87@gmail.com>
2018-09-17 18:13:27 +0900

    포스트 읽기 API 함수 생성

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,3 +4,5 @@ import axios from 'axios';
 // 글 작성 API 함수
 export const writePost = ({title, body, tags}) => axios.post('/api/posts', {title, body, tags});
 
+// 포스트 읽기 API 함수
+export const getPost = (id) => axios.get(`/api/posts/${id}`);
