296394a40177ee8cc5b66eb7e612db6d9eb28730
hellomac87 <hellomac87@gmail.com>
2018-09-17 18:09:35 +0900

    EditorHeaderContainer 컴포넌트 생성

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import EditorTemplate from "components/editor/EditorTemplate/EditorTemplate";
-import EditorHeader from "components/editor/EditorHeader/EditorHeader";
+import EditorHeaderContainer from "containers/editor/EditorHeaderContainer";
 import EditorContainer from "containers/editor/EditorPaneContainer";
 import PreviewPaneContainer from "containers/editor/PreviewPaneContainer";
 
 const EditorPage = () => {
     return (
         <EditorTemplate
-            header={<EditorHeader/>}
+            header={<EditorHeaderContainer/>}
             editor={<EditorContainer/>}
             preview={<PreviewPaneContainer/>}
         />
