1979e54d8bdca00573f9460debcaf6367fd24e4c
hellomac87 <hellomac87@gmail.com>
2018-09-20 11:44:51 +0900

    ModalWrapper, AskRemoveModal 컴포넌트 생성

---
+++ src/components/modal/AskRemoveModal/AskRemoveModal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "./AskRemoveModal.scss";
+import classNames from "classnames/bind";
+import ModalWrapper from "components/modal/Modal/ModalWrapper/ModalWrapper";
+
+const cx = classNames.bind(styles);
+
+const AskRemoveModal = () => {
+    return (
+        <ModalWrapper>
+            내용
+        </ModalWrapper>
+    );
+};
+
+export default AskRemoveModal;
